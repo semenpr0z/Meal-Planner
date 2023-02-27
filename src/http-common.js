55a3db14cf8e4f1d9d4c2976738207baf6dc79d6
Semen Prozov <justpr0z.com.ru@gmail.com>
2023-02-27 16:25:51 +0300

    added registration/login deploy

--- src/http-common.js
+++ src/http-common.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
-export default axios.create({
-    baseURL: "http://localhost:9090/",
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
-
 // export default axios.create({
-//     baseURL: "https://63f0ad9c5703e063fa4a4a59.mockapi.io/api/",
+//     baseURL: "http://localhost:9090/",
 //     headers: {
 //         'Content-Type': 'application/json'
 //     }
 // })
+
+export default axios.create({
+    baseURL: "https://63f0ad9c5703e063fa4a4a59.mockapi.io/api/",
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
